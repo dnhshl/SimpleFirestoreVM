c3fb1dc441241ecb92746db71796e6146dfd2869
Detlev Noll <dn@experikus.de>
2023-08-18 13:33:31 +0200

    ServerTimestamp added

--- app/src/main/java/com/example/simplefirestorevm/firestore/Sensordata.kt
+++ app/src/main/java/com/example/simplefirestorevm/firestore/Sensordata.kt
@@ -1,12 +1,14 @@
 package com.example.simplefirestorevm.firestore
 
 import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FieldValue
 
 class Sensordata (
     var location: String = "",
     var temperature: Int = 0,
     var humidity: Int = 0,
-    var timestamp: Timestamp = Timestamp.now()
+    var timestamp: Timestamp = Timestamp.now(),
+    var created_at: FieldValue = FieldValue.serverTimestamp()
 ) {
     override fun toString(): String {
         val datestring = convertTimestampToDateTimeString(timestamp)
